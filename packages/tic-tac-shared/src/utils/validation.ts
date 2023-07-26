c57b1902a458bf59dd3be3001f37a7174c9bf525
Michał Warać <49625375+auto200@users.noreply.github.com>
2023-07-26 21:55:02 +0200

    refactor: reuse env validator to common package (#30)

--- packages/tic-tac-shared/src/utils/validation.ts
+++ packages/tic-tac-shared/src/utils/validation.ts
@@ -1,4 +1,4 @@
-import { z } from "zod";
+import { ZodObject, ZodRawShape, z } from "zod";
 
 export const socketEventPayloadMakeMoveSchema = z.object({
     selectedPieceId: z.string(),
@@ -6,3 +6,15 @@ export const socketEventPayloadMakeMoveSchema = z.object({
 });
 
 export type SocketEventPayloadMakeMove = z.infer<typeof socketEventPayloadMakeMoveSchema>;
+
+export function validateEnv<T extends ZodObject<ZodRawShape>>(
+    schema: T,
+    envs: Record<string, string | undefined> = process?.env || {},
+): z.infer<T> {
+    const result = schema.safeParse(envs);
+
+    if (!result.success) {
+        throw new Error(`Configuration validation error, ${result.error.message}`);
+    }
+    return result.data;
+}
